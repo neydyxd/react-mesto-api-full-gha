cf0ae565c2281e4bedcea716575b16167376e75b
Stepan Maksimov <neydyxd@yandex.ru>
2023-06-28 16:56:05 +0300

    Поправил isLiked

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -6,7 +6,7 @@ import { useContext } from "react";
 function Card({ card, onCardClick, onCardLike, onCardDelete }) {
   const currentUser = useContext(CurrentUserContext);
   const isOwn = currentUser._id === card.owner;
-  const isLiked = card.likes.some((i) => i._id === currentUser._id);
+  const isLiked = card.likes.some((i) => i === currentUser._id);
   const cardLikeButtonClassName = `poster__like-button ${
     isLiked && "poster__like-button_active"
   }`;
