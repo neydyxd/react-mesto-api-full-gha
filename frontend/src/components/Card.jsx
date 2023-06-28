95b189697ce7208385f4c6df78c9ef8f1295ae02
Stepan Maksimov <neydyxd@yandex.ru>
2023-06-28 18:53:11 +0300

    проверка 2

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -6,6 +6,7 @@ import { useContext } from "react";
 function Card({ card, onCardClick, onCardLike, onCardDelete }) {
   const currentUser = useContext(CurrentUserContext);
   const isOwn = currentUser._id === card.owner;
+  console.log(card);
   console.log(card.likes);
   const isLiked = card.likes.some(i => i._id === currentUser._id);
   const cardLikeButtonClassName = `poster__like-button ${
